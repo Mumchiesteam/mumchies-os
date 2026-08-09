59963bbed0903d2081580c346002390fe10109c2
Sushil Kothari <mumchiesteam@gmail.com>
2026-08-10 02:45:47 +0530

    fix: shorten poller audit revision id

--- backend/alembic/versions/20260810_03_tracking_poller_audit.py
+++ backend/alembic/versions/20260810_03_tracking_poller_audit.py
@@ -1,6 +1,6 @@
 """add bounded shipment tracking poller audit
 
-Revision ID: 20260810_03_tracking_poller_audit
+Revision ID: 20260810_03_poller_audit
 Revises: 20260810_02_shipment_events
 """
 
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "20260810_03_tracking_poller_audit"
+revision = "20260810_03_poller_audit"
 down_revision = "20260810_02_shipment_events"
 branch_labels = None
 depends_on = None
